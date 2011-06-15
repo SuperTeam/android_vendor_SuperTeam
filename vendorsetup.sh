42466d853f73cbb37b6a681c513051c09abe590c
Superatmel <superatmel@gmail.com>
2011-06-15 14:53:31 +0200

    Añadido Vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,13 @@
+add_lunch_combo team_crespo-eng
+add_lunch_combo team_dream_sapphire-eng
+add_lunch_combo team_galaxysmtd-eng
+add_lunch_combo team_eeksphone-eng
+add_lunch_combo team_hero-eng
+add_lunch_combo team_passion-eng
+add_lunch_combo team_zeppelin-eng
+add_lunch_combo team_zero-eng
+add_lunch_combo team_morrison-eng
+PATH=$PATH:$PWD/vendor/Superteam/tools ; export PATH
+VENDOR=$PWD/vendor/SuperTeam
+
+#add_lunch_combo team_galaxytab-eng
