e8dfc28d3efcc9cf3f722c3274e3442aa76eb24f
podxboq <podxboq@gmail.com>
2011-06-18 12:54:50 +0200

    Nueva utilidad: 1.- Gestiona el entorno de compilación. 2.- Gestiona los ficheros cambiados. 3.- Sincroniza la última versíon con el dispositivo

---
+++ tools/build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#Inicializamos las variables
+TOPDIR=`pwd`
+DEVICE=$1
+ROMDIR=$TOPDIR/rom/$DEVICE
+UNZIPDIR=$ROMDIR/last_build
+RELEASEDIR=$ROMDIR/last_release
+
+option=0
+while [ ! $option -eq 3 ]; do
+	echo "Elige una opción para compilar:"
+	echo "1: make + squisher"
+	echo "2: squisher"
+	echo "3: salir"
+	read option
+	if [ $option -eq 3 ]; then
+		exit 0
+	fi
+	
+	if [ "$OUT" = "" ]; then
+		. build/envsetup.sh
+		lunch team_$DEVICE-eng
+	fi
+	
+	if [ $option -eq 1 ]; then
+	    make otapackage
+	fi
+	
+	vendor/SuperTeam/tools/squisher
+	
+	if [ -d $UNZIPDIR ]; then
+		rm -r $UNZIPDIR
+	fi
+	
+	mk -p $UNZIPDIR
+	for f in `ls $OUT/SuperOSR*.zip`; do
+	    unzip -d $UNZIPDIR/ $f
+	done  
+	
+	exec diff -qr $UNZIPDIR/system $RELEASEDIR/system | sort > $ROMDIR/diff.txt
+	
+#borramos los ficheros que no están y copiamos los cambiados.
+	adb remount
+	while read line; do
+	#echo "adb push $line ${line:${#UNZIPDIR}}"
+	    if [[ "$line" =~ "$UNZIPDIR" ]]; then
+	    	base=${line#*$UNZIPDIR*}
+	        if [[ "$line" =~ "Only" ]]; then
+	            file=${base/: //}
+	        else
+	            file=${base%* and*}
+	        fi
+	        accion=copiar
+	    else
+	        base=${line#*$RELEASEDIR*}
+	        file=${base/: //}
+	        accion=borrar
+	    fi
+	    if [[ $accion == "copiar" ]]; then
+	    	echo "adb push $UNZIPDIR$file $file"
+	    fi
+	    if [[ $accion == "borrar" ]]; then
+	    	echo "adb exec rm -r $file"
+	    fi 
+	done < $ROMDIR/diff.txt	
+	adb remount
+done
+	
