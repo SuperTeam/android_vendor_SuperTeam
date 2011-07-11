9f19453a94f57f9764cd4c993352ca1d554a79a6
Alexandre Dumont <adumont@gmail.com>
2011-07-11 15:37:18 -0700

    personalizar.sh for morrison & zeppelin
    Change-Id: Idf103342732e5e9dcee5a9a73b0773a2c83f37ec

---
+++ mods/terminales/zeppelin/personalizar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $1
+
+# No queremos ejecutar este script al incio de la ROM.
+#rm $1/system/bin/fix_permissions
+
+cd system
+sed -ie "s/ro.product.device=zeppelin/ro.product.device=zepp/" build.prop
