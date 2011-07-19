9c93d5fc3c38de9342092f2164b83a90159f6e11
superatmel <superatmel@gmail.com>
2011-07-19 22:10:44 +0200

    Añadido soporte para Galaxy S2
    Change-Id: Iebfd3f72d5526fd9328e01e7d584c6fb1d787a2b

---
+++ products/team_galaxys2.mk
@@ -0,0 +1,60 @@
+# Inherit AOSP device configuration for galaxys2.
+$(call inherit-product, device/samsung/galaxys2/full_galaxys2.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/SuperTeam/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/SuperTeam/products/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := team_galaxys2
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := galaxys2
+PRODUCT_MODEL := GT-I9100
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:2.3.3/GINGERBREAD/XWKE7:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 2.3.3 GINGERBREAD XWKE7 release-keys"
+
+# Extra captivate overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/SuperTeam/overlay/galaxys2
+
+# Add FM and Torch Apps
+PRODUCT_PACKAGES += \
+    DSPManager \
+    Launcher2 \
+    Torch \
+    FM
+
+# Extra RIL settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.enable.managed.roaming=1 \
+    ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
+    ro.ril.emc.mode=2
+
+# Add additional mounts
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.additionalmounts=/mnt/sdcard/external_sd
+
+#
+# Set ro.modversion
+#
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=SuperOSR-ST@-GalaxyS2-S-1-AW \
+    ro.stats.romversion=1 \
+    ro.config.play.bootsound=0
+
+#PRODUCT_LOCALES := \
+#    es_ES \
+#    ca_ES \
+#    en_US \
+#    de_DE \
+#    eu_ES \
+#    fr_FR \
+#    it_IT \
+#    pt_PT \
+#    ru_RU \
+#    hdpi
+
+#PRODUCT_DEFAULT_LANGUAGE := es_ES
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo team_crespo-eng
 add_lunch_combo team_dream_sapphire-eng
+add_lunch_combo team_galaxys2-eng
 add_lunch_combo team_galaxysmtd-eng
 add_lunch_combo team_galaxytab-eng
 add_lunch_combo team_one-eng
@@ -12,5 +13,3 @@ add_lunch_combo team_p990-eng
 #add_lunch_combo team_p970-eng
 PATH=$PATH:$PWD/vendor/SuperTeam/tools ; export PATH
 VENDOR=$PWD/vendor/SuperTeam
-
-#add_lunch_combo team_galaxytab-eng
