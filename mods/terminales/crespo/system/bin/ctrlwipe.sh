6af35a4e20511be3ccc0ba29b27c9e267f0fbb34
superatmel <superatmel@gmail.com>
2011-07-19 22:09:58 +0200

    ctrlwipe: Añadidos archivos que faltan para el control de wipe
    Change-Id: I70cbf814a6229fc959eb71fba6975aa7fa08da5a

--- mods/terminales/crespo/system/bin/ctrlwipe.sh
+++ mods/terminales/crespo/system/bin/ctrlwipe.sh
@@ -4,6 +4,8 @@
 #como si fuera una ROM recién instalada
 echo " "
 echo "Recuperacion de datos personalizada"
+cp /system/backup/configsup.cf /data/configsup.cf;
+cp /system/backup/sup.config /data/sup.config;
 cp /system/backup/bln /data/app/neldar.bln.control.free.apk;
 cp /system/backup/flash /data/app/com.adobe.flashplayer-1.apk;
 cp /system/backup/honey /data/app/HoneyBread-Alpha-V4-signed.apk;
