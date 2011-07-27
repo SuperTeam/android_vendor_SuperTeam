fbf587d2213606d97cae8a511c5482deb69b9956
podxboq <podxboq@gmail.com>
2011-07-27 22:39:47 +0200

    preparando para sync
    Change-Id: Ib73dd2e7928ab59f400d97c480fc22a37e4cfb1e

--- tools/reposync.sh
+++
@@ -1,4 +0,0 @@
-cd $HOME/src/wsAndroid/SuperTeam
-repo sync
-find . -path './rom' -prune -o -path './out' -prune -o -path '*/.git' -prune -o -path './.repo' -prune -o \! -type d -mtime -1 -print >> ../podxboq/out/cambios.txt
-#for f in `cat ../podxboq/out/cambios.txt`; do cp ../SuperTeam/$f $f; done
