0791392ad5b86819512e7249767319b935b29b4b
nelo360 <nelo360@gmail.com>
2011-07-28 14:17:04 +0200

    Passion: Fix 2 personalizar.sh
    Change-Id: Id314c3923bd23283b10f3d6fe0665a415dba95e3

--- mods/terminales/passion/personalizar.sh
+++ mods/terminales/passion/personalizar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Eliminamos algunos apk's para dejar espacio en system
-rm -rf $OUT/repack.d/ota/system/app/LatinIME.apk
+rm -rf $OUT/repack.d/ota/system/app/Cyanbread.apk
 rm -rf $OUT/repack.d/ota/system/app/Androidian.apk
