9053587e8a0a10682591afdb52ddac2f0fbfa0b8
nelo360 <nelo360@gmail.com>
2011-07-28 14:15:50 +0200

    Passion: Fix personalizar.sh
    Change-Id: I2a949d00ec6bcf7ee7f2ea5d4946bf38005c37b4

--- mods/terminales/passion/personalizar.sh
+++ mods/terminales/passion/personalizar.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Eliminamos algunos apk's innecesarios y cambio de kernel
+# Eliminamos algunos apk's para dejar espacio en system
 rm -rf $OUT/repack.d/ota/system/app/LatinIME.apk
-rm -rf $OUT/repack.d/ota/system/app/Torch.apk
-rm -rf $OUT/repack.d/ota/system/etc/super/04superteam
-rm -rf $OUT/repack.d/ota/boot.img
-cp $OUT/kernel $OUT/repack.d/ota/zImage
-cp $ANDROID_BUILD_TOP/device/samsung/galaxytab/Kernel_instalacion.zip $OUT/Kernel_instalacion.zip
+rm -rf $OUT/repack.d/ota/system/app/Androidian.apk
