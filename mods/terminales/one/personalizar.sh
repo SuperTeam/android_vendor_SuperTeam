a7ed36c211c2c11b9d694de627e43562e7c058f7
superatmel <superatmel@gmail.com>
2011-08-27 01:20:49 +0200

    Actualizadas algunas Google Apps, Cambio de version para Zero y One, añadidas algunas apks, cambio de lugar bootanimation para One, etc...
    Change-Id: Ia74b4ff457114d9fd905b3e42d897659306eeffb

--- mods/terminales/one/personalizar.sh
+++ mods/terminales/one/personalizar.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-# No queremos ejecutar este script al incio de la ROM.
+cd $1
+# Eliminamos algunos apk's innecesarios
+cd system
+rm $1/system/etc/super/04superteam
 rm $1/system/bin/fix_permissions
+rm $OUT/repack.d/ota/system/media/bootanimation.zip
