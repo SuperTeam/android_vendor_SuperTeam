18d923157ba1311037f6323c90621c83dfd6b4f7
superatmel <superatmel@gmail.com>
2011-08-26 18:39:18 +0200

    Cambio de version en Nexus S
    Change-Id: If3f8d489d2d6fbfc629e037dba9c0fb901aaf217

--- products/team_crespo.mk
+++ products/team_crespo.mk
@@ -41,7 +41,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.data.on=0 \
-    ro.modversion=SuperOSR-ST@-Nexus-S-1-AW \
+    ro.modversion=SuperOSR-ST@-Nexus-S-2-AW \
     ro.stats.romversion=1 \
     ro.config.play.bootsound=0
 
