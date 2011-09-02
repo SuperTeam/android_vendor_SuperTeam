d7818ee8e77a5317fa9ec07e6c7d0262235cb766
superatmel <superatmel@gmail.com>
2011-09-02 14:41:08 +0200

    Galaxy S2:Edicion de personalizar.sh y actualizado backup
    Change-Id: I04976d5a79bf33d5d95e15625da4664538504667

--- mods/terminales/galaxys2/personalizar.sh
+++ mods/terminales/galaxys2/personalizar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd $1
-# Eliminamos algunos apk's innecesarios
-cd system
-rm $1/system/app/Torch.apk
-rm $1/system/etc/super/04superteam
+. mensajes.sh
+msgStatus "Personalizacion exclusiva"
+msgStatus " "
+rm -rf $OUT/repack.d/ota/system/app/BooksPhone.apk
+rm -rf $OUT/repack.d/ota/system/app/Torch.apk
+rm -rf $OUT/repack.d/ota/system/etc/super/04superteam
