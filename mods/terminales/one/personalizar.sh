1f471323638729dbc1f29b64019d556db4388a9e
superatmel <superatmel@gmail.com>
2011-09-02 14:39:46 +0200

    Eliminacion de anti-wipe y mucho mas agresivo la limpieza de System
    Change-Id: Ic48c9b7213fc98ed1ecb2f1a34f0eca2c2f5b7c1

--- mods/terminales/one/personalizar.sh
+++ mods/terminales/one/personalizar.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-cd $1
-# Eliminamos algunos apk's innecesarios
-cd system
-rm $1/system/etc/super/04superteam
-rm $1/system/bin/fix_permissions
+. mensajes.sh
+msgStatus "Personalizacion exclusiva"
+msgStatus " "
+# Eliminamos algunos archivos innecesarios
+mv -f $OUT/repack.d/ota/system/app/BatCircular.apk $OUT/repack.d/ota/data/app/BatCircular.apk
+mv -f $OUT/repack.d/ota/system/app/BatCircular2.apk $OUT/repack.d/ota/data/app/BatCircular2.apk
+mv -f $OUT/repack.d/ota/system/app/BatNelo.apk $OUT/repack.d/ota/data/app/BatNelo.apk
+mv -f $OUT/repack.d/ota/system/app/BatNumerada.apk $OUT/repack.d/ota/data/app/BatNumerada.apk
+rm -rf $OUT/repack.d/ota/system/app/BooksPhone.apk
+mv -f $OUT/repack.d/ota/system/app/Vending.apk $OUT/repack.d/ota/data/app/Vending.apk
+mv -f $OUT/repack.d/ota/system/app/Wallpapers.apk $OUT/repack.d/ota/data/app/Wallpapers.apk
+rm -rf $OUT/repack.d/ota/system/backup
+rm -rf $OUT/repack.d/ota/system/etc/super/03ctrlwipe
+rm -rf $OUT/repack.d/ota/system/etc/super/04superteam
+rm -rf $OUT/repack.d/ota/system/media/audio/ringtones/Theway.mp3
+rm -rf $OUT/repack.d/ota/system/media/audio/ringtones/CrazyDream.ogg
+rm -rf $OUT/repack.d/ota/system/media/audio/ringtones/DreamTheme.ogg
+rm -rf $OUT/repack.d/ota/system/media/audio/ringtones/URSAMINOR.ogg
 rm $OUT/repack.d/ota/system/media/bootanimation.zip
+
+#Corregimos build.prop
+msgStatus "Corregimos build.prop"
+msgStatus " "
+sed -i 's/ro.config.ringtone=Theway.mp3/ro.config.ringtone=Old_Phone.mp3/g' $OUT/repack.d/ota/system//build.prop
