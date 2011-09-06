0204cabfd3c57ca0d9debff9af68423a1d130dd0
superatmel <superatmel@gmail.com>
2011-09-06 11:33:22 +0200

    Nexus S:Subida version
    Change-Id: Ic0bd9b08c55fc9cc51bed704667a30effce2d29e

--- products/team_crespo.mk
+++ products/team_crespo.mk
@@ -41,8 +41,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.data.on=0 \
-    ro.modversion=SuperOSR-ST@-Nexus-S-2.0.2-AW \
-    ro.stats.romversion=2.0.2 \
+    ro.modversion=SuperOSR-ST@-Nexus-S-2.0.4-AW \
+    ro.stats.romversion=2.0.4 \
     ro.config.play.bootsound=0
 
 #PRODUCT_LOCALES := \
