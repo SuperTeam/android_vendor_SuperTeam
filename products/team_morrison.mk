c881e8957148a009c4c10104831dfa064ffb9cd1
Alexandre Dumont <adumont@gmail.com>
2011-09-06 16:55:04 -0700

    morr/zepp: version 2.0.0 in product files
    Change-Id: I823ffb1a1dae3c444da418fa8cc4abaa9054ad2e

--- products/team_morrison.mk
+++ products/team_morrison.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += FM
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.data.on=0 \
     ro.modversion=SuperOSR-morrison-2.0.0 \
-    ro.stats.romversion=1.0b2 \
+    ro.stats.romversion=2.0.0 \
     ro.config.play.bootsound=0
 
 PRODUCT_LOCALES := \
