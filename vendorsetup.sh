dfad505b8deb70269138f49e619a6f77a2052fd2
Alorma <bernatbor15@gmail.com>
2011-09-11 16:33:06 +0200

    p970 añadido
    Change-Id: Idc65a697bcc23a8ee6741582844f73cecf8e1d4f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,6 +10,6 @@ add_lunch_combo team_zero-eng
 add_lunch_combo team_morrison-eng
 add_lunch_combo team_p990-eng
 add_lunch_combo team_boston-eng
-#add_lunch_combo team_p970-eng
+add_lunch_combo team_p970-eng
 PATH=$PATH:$PWD/vendor/SuperTeam/tools ; export PATH
 VENDOR=$PWD/vendor/SuperTeam
