643ae8a8fee716eaa2e1273ea840c2182ec09368
podxboq <podxboq@gmail.com>
2011-09-11 18:12:22 +0200

    NEW: Format preferences
    Change-Id: Ib7835e61f50d853a0c233fe69cae2af3c80a4d80

--- products/team_dream_sapphire.mk
+++ products/team_dream_sapphire.mk
@@ -1,15 +1,9 @@
-# Inherit AOSP device configuration for dream_sapphire.
 $(call inherit-product, device/htc/dream_sapphire/full_dream_sapphire.mk)
 
-# Inherit some common SuperTeam stuff.
 $(call inherit-product, vendor/SuperTeam/products/common.mk)
 
-# Añadimos los temas
 $(call inherit-product, vendor/SuperTeam/products/themes_small.mk)
 
-#
-# Setup device specific product configuration.
-#
 PRODUCT_NAME := team_dream_sapphire
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := dream_sapphire
@@ -17,13 +11,10 @@ PRODUCT_MODEL := Dream/Sapphire
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=tmobile/opal/sapphire/sapphire:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="opal-user 2.2.1 FRG83 60505 release-keys"
 
-# Extra Dream/Sapphire overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/SuperTeam/overlay/dream_sapphire
 
-# Include apps
 PRODUCT_PACKAGES += Gallery
 
-# Enable Compcache by default on D/S
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.data.on=0 \
 	ro.modversion=SuperOSR-ST@-dream_sapphire-2.0.0 \
@@ -35,10 +26,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.product.multi_touch_enabled=true \
 	ro.setupwizard.mode=DISABLED
   
-# Use the audio profile hack
 WITH_DS_HTCACOUSTIC_HACK := true
 
-#Añadimos permisos extra
 PRODUCT_COPY_FILES +=  \
   frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml 
 
--- products/team_p990.mk
+++ products/team_p990.mk
@@ -1,25 +1,21 @@
-# Inherit device configuration
 $(call inherit-product, device/lge/p990/p990.mk)
 
-# Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/SuperTeam/products/common_full.mk)
 
-#
-# Setup device specific product configuration.
-#
 PRODUCT_NAME := team_p990
 PRODUCT_BRAND := lge
 PRODUCT_DEVICE := p990
 PRODUCT_MODEL := Optimus 2x
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_star BUILD_ID=FRG83G BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=lge/lge_star/p990/p990:2.2.2/FRG83G/lgp990-V10b.2ED2ADCFFC:user/release-keys PRIVATE_BUILD_DESC="star-user 2.2.2 FRG83G 2ED2ADCFFC release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=lge_star \
+	BUILD_ID=FRG83G \
+	BUILD_DISPLAY_ID=GRJ90 \
+	BUILD_FINGERPRINT=lge/lge_star/p990/p990:2.2.2/FRG83G/lgp990-V10b.2ED2ADCFFC:user/release-keys \
+	PRIVATE_BUILD_DESC="star-user 2.2.2 FRG83G 2ED2ADCFFC release-keys"
 
-# Extra lg overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/SuperTeam/overlay/p990
 
-#
-# Set ro.modversion
-#
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=SuperOSR-ST@-Optimus2x-2.0.0 \
     ro.stats.romversion=2.0.0 \
@@ -27,5 +23,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.play.bootsound=0
 
 PRODUCT_PACKAGES += DSPManager Torch 
-
-#TARGET_BUILD_TYPE := release
