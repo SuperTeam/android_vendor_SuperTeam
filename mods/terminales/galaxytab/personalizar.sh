0e74c51b3d8463226a2d0fbb7673ebf883d5d646
superatmel <superatmel@gmail.com>
2011-09-14 12:24:28 +0200

    Galaxy Tab: Subida version y solucion SDcard externa
    Change-Id: I5825355f4c890a5c4871da6c0ace8b8ae4fd1b32

--- mods/terminales/galaxytab/personalizar.sh
+++ mods/terminales/galaxytab/personalizar.sh
@@ -3,6 +3,7 @@
 msgStatus "Personalizacion exclusiva"
 msgStatus " "
 # Eliminamos algunos archivos innecesarios
+rm -rf $OUT/repack.d/ota/system/app/GlobalTime.apk
 rm -rf $OUT/repack.d/ota/system/app/LatinIME.apk
 rm -rf $OUT/repack.d/ota/system/app/Torch.apk
 rm -rf $OUT/repack.d/ota/system/etc/super/04superteam
