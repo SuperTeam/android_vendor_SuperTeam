9253ce211d73cfab1d96025349eaefca2ae02637
superatmel <superatmel@gmail.com>
2011-09-15 12:51:00 +0200

    Modificacion de forma de trabajo de control Wipe
    Change-Id: I4490836b357fe5515045b71fdfa6d186b0710354

--- mods/terminales/morrison/system/bin/ctrlwipe.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-#
-#Llamade desde el control de WIPE, recupera la parte específica del terminal 
-#como si fuera una ROM recién instalada
-echo " "
-echo "Recuperacion de datos personalizada"
-cp /system/backup/tether /data/app/android.tether.apk;
-sleep 1;
