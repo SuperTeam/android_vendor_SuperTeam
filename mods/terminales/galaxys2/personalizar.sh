b114ae4e29a49ed9ffbfaffcff5ed7e91e1edc6b
superatmel <superatmel@gmail.com>
2011-09-26 11:04:33 +0200

    Ahora se compila Torch
    Change-Id: Id5efce81a9aa1050813645b20fc023bf7b9c718c

--- mods/terminales/galaxys2/personalizar.sh
+++ mods/terminales/galaxys2/personalizar.sh
@@ -4,5 +4,4 @@ msgStatus "Personalizacion exclusiva"
 msgStatus " "
 rm -rf $OUT/repack.d/ota/system/app/BooksPhone.apk
 rm -rf $OUT/repack.d/ota/system/app/GlobalTime.apk
-rm -rf $OUT/repack.d/ota/system/app/Torch.apk
 rm -rf $OUT/repack.d/ota/system/etc/super/04superteam
