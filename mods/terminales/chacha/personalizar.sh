aa0d9c483cdf548562e0d0c53d078e6626180766
Alexandre Dumont <adumont@gmail.com>
2011-10-23 01:46:18 +0200

    chacha: changes in personalizar.sh
    Change-Id: I2dc8f836908c6977efa856275d41b8953e4e8876

--- mods/terminales/chacha/personalizar.sh
+++ mods/terminales/chacha/personalizar.sh
@@ -8,10 +8,10 @@ ROOTDIR=$1
 # rm $1/system/bin/fix_permissions
 
 cd $ROOTDIR/system
-#echo "Some build.prop edits"
+echo "Some build.prop edits"
 #sed -i -e "s/ro.product.device=morrison/ro.product.device=morr/" build.prop
-#sed -i -e "s/^ro.product.locale.language=.*/ro.product.locale.language=en/" build.prop
-#sed -i -e "s/^ro.product.locale.region=.*/ro.product.locale.region=US/" build.prop
+sed -i -e "s/^ro.product.locale.language=.*/ro.product.locale.language=en/" build.prop
+sed -i -e "s/^ro.product.locale.region=.*/ro.product.locale.region=US/" build.prop
 
 echo "Removing some big files to make space"
 cd $ROOTDIR/system/app
@@ -23,6 +23,9 @@ cd $ROOTDIR/system/app
 rm LatinImeTutorial.apk
 rm Development.*
 rm BooksPhone.apk
+
+# LatinIME FC's on the Chacha. Anyway useless with physical kbd
+rm LatinIME.apk
 
 #cd $ROOTDIR/system/media
 #mkdir -p $ROOTDIR/data/local/
