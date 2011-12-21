a1b2c12704e22c23ef7b84653c3bd6f17e79b21c
podxboq <podxboq@gmail.com>
2011-12-21 15:06:01 +0100

    Upgrade version

--- products/team_p990.mk
+++ products/team_p990.mk
@@ -17,8 +17,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_PACKAGE_OVERLAYS += vendor/SuperTeam/overlay/p990
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=SuperOSR-ST@-Optimus2x-2.2.2 \
-    ro.stats.romversion=2.2.2 \
+    ro.modversion=SuperOSR-ST@-Optimus2x-2.4.0 \
+    ro.stats.romversion=2.4.0 \
     ro.data.on=0 \
     ro.config.play.bootsound=0
 
