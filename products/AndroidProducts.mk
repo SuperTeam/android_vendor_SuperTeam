1d0eb7c3a7b0c0275d61614d08e2105243291b03
superatmel <superatmel@gmail.com>
2012-01-25 15:34:25 +0100

    Fix project
    Change-Id: If3a10bdca83996166706a9ca19080f88df3fd69e

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,19 +3,19 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/team_anzu.mk \
 	$(LOCAL_DIR)/team_crespo.mk \
 	$(LOCAL_DIR)/team_dream_sapphire.mk \
-        $(LOCAL_DIR)/team_galaxys2.mk \
-        $(LOCAL_DIR)/team_galaxysmtd.mk \
-        $(LOCAL_DIR)/team_galaxytab.mk \
-        $(LOCAL_DIR)/team_morrison.mk \
-        $(LOCAL_DIR)/team_morrison_cn.mk \
-        $(LOCAL_DIR)/team_one.mk \
-        $(LOCAL_DIR)/team_passion.mk \
-        $(LOCAL_DIR)/team_zeppelin.mk \
-        $(LOCAL_DIR)/team_zeppelin_cn.mk \
+	$(LOCAL_DIR)/team_galaxys2.mk \
+	$(LOCAL_DIR)/team_galaxysmtd.mk \
+	$(LOCAL_DIR)/team_galaxytab.mk \
+	$(LOCAL_DIR)/team_morrison.mk \
+	$(LOCAL_DIR)/team_morrison_cn.mk \
+	$(LOCAL_DIR)/team_one.mk \
+	$(LOCAL_DIR)/team_passion.mk \
+	$(LOCAL_DIR)/team_zeppelin.mk \
+	$(LOCAL_DIR)/team_zeppelin_cn.mk \
 	$(LOCAL_DIR)/team_p990.mk \
 	$(LOCAL_DIR)/team_zero.mk \
 	$(LOCAL_DIR)/team_boston.mk \
-        $(LOCAL_DIR)/team_p970.mk \
--       $(LOCAL_DIR)/team_chacha.mk \
-        $(LOCAL_DIR)/team_bravo.mk \
-        $(LOCAL_DIR)/team_sirius.mk
+	$(LOCAL_DIR)/team_p970.mk \
+	$(LOCAL_DIR)/team_chacha.mk \
+	$(LOCAL_DIR)/team_bravo.mk \
+	$(LOCAL_DIR)/team_sirius.mk
