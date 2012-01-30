cca92245352b824e753f9345111f95b88b7aa40b
superatmel <superatmel@gmail.com>
2012-01-30 01:44:20 +0100

    Sirius:Not neccesary
    Change-Id: I20d789f1c34c47d3efdf5be92fbc5ea579a09bfa

--- mods/terminales/sirius/personalizar.sh
+++ mods/terminales/sirius/personalizar.sh
@@ -4,3 +4,4 @@ msgStatus "Personalizacion exclusiva"
 rm $OUT/repack.d/ota/boot.img
 rm -rf $OUT/repack.d/ota/system/app/SpareParts.*
 rm -rf $OUT/repack.d/ota/system/app/SpareParts2.*
+rm -rf $OUT/repack.d/ota/system/etc/super/04superteam
