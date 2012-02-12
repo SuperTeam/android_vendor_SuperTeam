4c0c14dd03c8b3d8cdbbb5e0e8b19841dbceb005
nelo360 <nelo360@gmail.com>
2012-02-12 15:17:32 +0100

    PASSION: Update rom version, deleted gapps on /data
    Change-Id: I028bf8e2e1ac74860f7a42dd3b0122d72d5cbe8a

--- mods/terminales/passion/personalizar.sh
+++ mods/terminales/passion/personalizar.sh
@@ -7,6 +7,7 @@ rm -rf $OUT/repack.d/ota/system/app/BooksPhone.apk
 rm -rf $OUT/repack.d/ota/system/app/GlobalTime.apk
 rm -rf $OUT/repack.d/ota/system/app/SpareParts.apk
 rm -rf $OUT/repack.d/ota/system/app/SpareParts2.apk
+
 # Borramos 02permisos para poder utilizar uno especial para passion
 rm -rf $OUT/repack.d/ota/system/etc/super2/02permisos
 
